7f49258989c0b285f017fc94004a6b3df637fb1b
max-well-d <45420132+max-well-d@users.noreply.github.com>
2019-03-11 10:43:04 +0800

    Delete group_idtest.py

--- group_idtest.py
+++
@@ -1,6 +0,0 @@
-from nonebot import on_command, CommandSession
-@on_command("groupid")
-async def gd(session: CommandSession):
-	print (session.ctx["user_id"])
-	if session.ctx["message_type"] == "group": print (session.ctx["group_id"])
-	print (session.ctx["message_type"])
